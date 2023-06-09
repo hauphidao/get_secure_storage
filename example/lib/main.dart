abf193193d12b437a1649eac55f44a0917a9b7e8
Grant Slender <gslender@gmail.com>
2023-06-09 15:56:59 +1000

    improved example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_secure_storage/get_secure_storage.dart';
 
 void main() async {
-  await GetSecureStorage.init(password: 'password');
+  await GetSecureStorage.init(password: 'strongpassword');
   runApp(const App());
 }
 
@@ -15,9 +15,19 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final box = GetSecureStorage();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  String get lastupdated => box.read('lastupdated') ?? 'never';
+
   bool get isDark => box.read('darkmode') ?? false;
+
   void changeTheme(bool val) {
     box.write('darkmode', val);
+    box.write('lastupdated', DateTime.now().toLocal().toString());
     setState(() {});
   }
 
@@ -27,12 +37,27 @@ class _AppState extends State<App> {
       theme: isDark ? ThemeData.dark() : ThemeData.light(),
       home: Scaffold(
         appBar: AppBar(title: const Text("GetSecureStorage")),
-        body: Center(
-          child: SwitchListTile(
-            value: isDark,
-            title: const Text("Touch to change ThemeMode"),
-            onChanged: changeTheme,
-          ),
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(flex: 1),
+            Flexible(
+              flex: 3,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SwitchListTile(
+                    value: isDark,
+                    title: const Text("Touch to change ThemeMode"),
+                    onChanged: changeTheme,
+                  ),
+                  const SizedBox(height: 10),
+                  Text('Last updated: $lastupdated')
+                ],
+              ),
+            ),
+            const Spacer(flex: 1),
+          ],
         ),
       ),
     );
